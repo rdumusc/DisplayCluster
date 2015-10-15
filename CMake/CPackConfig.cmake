3217e0c5d3c476254b0da42adfe6f897acd00782
Stefan Eilemann <Stefan.Eilemann@epfl.ch>
2015-10-15 14:37:27 +0200

    Improved project info

--- CMake/CPackConfig.cmake
+++ CMake/CPackConfig.cmake
@@ -1,26 +1,13 @@
-# Copyright (c) BBP/EPFL 2011-2014
-# Stefan.Eilemann@epfl.ch
-# All rights reserved. Do not distribute without further notice.
+# Copyright (c) BBP/EPFL 2011-2015
+#                        Stefan.Eilemann@epfl.ch
 
-# General CPack configuration
-# Info: http://www.itk.org/Wiki/CMake:Component_Install_With_CPack
-
-set(CPACK_PACKAGE_EXECUTABLES "${STREAMER_APP_NAME}")
-set(CPACK_PACKAGE_NAME "${STREAMER_APP_NAME}")
-set(CPACK_PACKAGE_CONTACT "Daniel Nachbaur <daniel.nachbaur@epfl.ch>")
-set(CPACK_PACKAGE_VENDOR "http://www.tacc.utexas.edu/tacc-software/displaycluster")
-
-set(CPACK_COMPONENT_DESKTOPSTREAMER_DISPLAY_NAME "DesktopStreamer Application")
-set(CPACK_COMPONENT_DESKTOPSTREAMER_DESCRIPTION "DesktopStreamer is an application that lets you stream your desktop to a running DisplayCluster instance.")
+set(CPACK_PACKAGE_VENDOR "bluebrain.epfl.ch")
 
 set(CPACK_COMPONENT_CORE_DISPLAY_NAME "DisplayCluster Application")
 set(CPACK_COMPONENT_DEV_DISPLAY_NAME "Development headers and libraries")
 set(CPACK_COMPONENT_DOC_DISPLAY_NAME "Documentation")
 
-set(CPACK_COMPONENT_DESKTOPSTREAMER_DEPENDS dcstream)
-
-# Currently we only package desktopstreamer
-set(CPACK_COMPONENTS_ALL desktopstreamer dcstream)
+set(CPACK_COMPONENTS_ALL core dev doc)
 
 # Linux Debian specific settings
 set(CPACK_DEBIAN_PACKAGE_DEPENDS "libqt4-core, libqt4-gui, libqt4-network, libturbojpeg" )
@@ -28,4 +15,3 @@ set(CPACK_DEB_COMPONENT_INSTALL ON) # Set this to package only components in CPA
 set(CPACK_COMPONENTS_ALL_IN_ONE_PACKAGE 1) # Don't make a separate package for each component
 
 include(CommonCPack)
-include(GNUModules)
